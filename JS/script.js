deab504ec5c45ca3c61e56f4b23a4cc62b5ba6ac
Innocent-Mbanda <mbandainnocent@gmail.com>
2020-05-06 18:02:42 -0400

    added new files

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+// $('.js--section-features').waypoints(function(direction){
+// if(direction == "down"){
+//     $('nav').addClass('sticky');
+// }else{
+//    $('nav').removeClass('sticky') 
+// }
+
+// })
+
+
+
+
+// var waypoints = $('#handler-first').waypoint(function(direction) {
+//     notify(this.element.id + ' hit 25% from top of window') 
+//   }, {
+//     offset: '25%'
+//   })
+  
+
+
+
+
+// $(document).ready (function(){
+//     $("h3").click (function(){
+//     $(this).css ('background-color','#ff0000')
+//     })
+// })
