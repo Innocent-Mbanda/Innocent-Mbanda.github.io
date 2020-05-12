70013d23b2ca8adf72d68b6b01576db06d25588f
Innocent-Mbanda <mbandainnocent@gmail.com>
2020-05-11 23:02:34 -0400

    added nav sticky

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,14 @@
-// $('.js--section-features').waypoints(function(direction){
-// if(direction == "down"){
-//     $('nav').addClass('sticky');
-// }else{
-//    $('nav').removeClass('sticky') 
-// }
 
-// })
+/*for sticky jquerry*/
 
-
-
-
-// var waypoints = $('#handler-first').waypoint(function(direction) {
-//     notify(this.element.id + ' hit 25% from top of window') 
-//   }, {
-//     offset: '25%'
-//   })
-  
+$('.js--section-features').waypoint(function(direction){
+if(direction == "down"){
+    $('nav').addClass('sticky');
+}else{
+   $('nav').removeClass('sticky'); 
+}
+}, { offset: '60px;'
+});
 
 
 
-
-// $(document).ready (function(){
-//     $("h3").click (function(){
-//     $(this).css ('background-color','#ff0000')
-//     })
-// })
